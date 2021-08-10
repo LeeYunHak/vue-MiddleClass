b16aeb62acc460065541e18f9f5ed4525e873b4b
YunHak <sza0203@naver.com>
2021-08-10 21:49:50 +0900

    인풋 컴포넌트 코드 정리, UI 스타일링

--- vue-todo/src/components/TodoInput.vue
+++ vue-todo/src/components/TodoInput.vue
@@ -1,8 +1,11 @@
 <template>
-  <div>
+  <div class="inputBox shadow">
       <!-- v-model : 인풋에 입력된 텍스트값을 동적으로 바로 바로 뷰인스턴스 안에 맵핑하는 역할 -->
-      <input type="text" v-model="newTodoItem">
-      <button v-on:click="addTodo">add</button>
+      <input type="text" v-model="newTodoItem" v-on:keyup.enter="addTodo">
+      <!-- <button v-on:click="addTodo">add</button> -->
+      <span class="addContainer" v-on:click="addTodo">
+        <i class="fas fa-plus addBtn"></i>
+      </span>
   </div>
 </template>
 
@@ -15,17 +18,41 @@ export default {
   },
   methods: {
     addTodo: function(){
-      console.log(this.newTodoItem);
       // 저장하는 로직
       // localStorage.setItem(키, 벨류)
-      localStorage.setItem(this.newTodoItem, this.newTodoItem)
-
+      localStorage.setItem(this.newTodoItem, this.newTodoItem);
+      this.clearInput();
+    },
+    clearInput: function(){
       this.newTodoItem = ''; //입력하고 나서 입력창 초기화
     }
   }
 }
 </script>
 
-<style>
-
+<style scopred>
+input:focus{
+  outline: none;
+}
+.inputBox {
+  background: white;
+  height: 50px;
+  line-height: 50px;
+  border-radius: 5px;
+}
+.inputBox input {
+  border-style: none;
+  font-size: 0.9rem;
+}
+.addContainer {
+  float: right;
+  background: linear-gradient(to right, #6478FB, #8763FB);
+  display: block;
+  width: 3rem;
+  border-radius: 0 5px 5px 0;
+}
+.addBtn{
+  color: white;
+  vertical-align: middle;
+}
 </style>
