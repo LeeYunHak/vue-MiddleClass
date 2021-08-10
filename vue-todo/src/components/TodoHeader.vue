f4650cc7f6a725f2d33218c4900e11284f723160
YunHak <sza0203@naver.com>
2021-08-10 21:28:21 +0900

    해더 스타일링 및 로컬스토리지로 로컬에 저장

--- vue-todo/src/components/TodoHeader.vue
+++ vue-todo/src/components/TodoHeader.vue
@@ -1,15 +1,13 @@
 <template>
-  <div>
-      header
-  </div>
+  <header>
+    <h1>TODO it!</h1>
+  </header>
 </template>
-
-<script>
-export default {
-
+// 뷰에서 지원하는 속성 scoped 해당 컴포넌트 안에서만 유효한 css 속성
+<style scoped>
+h1 {
+  color: #2F3B52;
+  font-weight: 900;
+  margin: 2.5rem 0 1.5rem;
 }
-</script>
-
-<style>
-
 </style>
--- vue-todo/src/components/TodoInput.vue
+++ vue-todo/src/components/TodoInput.vue
@@ -1,12 +1,28 @@
 <template>
   <div>
-      input
+      <!-- v-model : 인풋에 입력된 텍스트값을 동적으로 바로 바로 뷰인스턴스 안에 맵핑하는 역할 -->
+      <input type="text" v-model="newTodoItem">
+      <button v-on:click="addTodo">add</button>
   </div>
 </template>
 
 <script>
 export default {
+  data : function(){
+    return {
+      newTodoItem: ""
+    }
+  },
+  methods: {
+    addTodo: function(){
+      console.log(this.newTodoItem);
+      // 저장하는 로직
+      // localStorage.setItem(키, 벨류)
+      localStorage.setItem(this.newTodoItem, this.newTodoItem)
 
+      this.newTodoItem = ''; //입력하고 나서 입력창 초기화
+    }
+  }
 }
 </script>
 
