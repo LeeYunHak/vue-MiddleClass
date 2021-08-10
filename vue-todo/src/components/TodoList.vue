6c7af4b5bd56e618a5f3c3cbcb6f8ca012cccece
YunHak <sza0203@naver.com>
2021-08-10 23:22:16 +0900

    todoList 컴포넌트 UI 스타일

--- vue-todo/src/components/TodoList.vue
+++ vue-todo/src/components/TodoList.vue
@@ -2,9 +2,12 @@
   <div>
       <ul>
         <!-- v-for 돌릴거 in 투두아이템 배열만큼 돌림 v-for를 쓸때는 v-bind:key를 추가해야함  -->
-        <li v-for="todoItem in todoItems" v-bind:key="todoItem">
+        <li v-for="todoItem in todoItems" v-bind:key="todoItem" class="shadow">
           <!-- 콧수염 괄호로 찍으면 됨 -->
           {{ todoItem }}
+          <span class="removeBtn" v-on:click="removeTodo">
+            <i class="fas fa-trash-alt"></i>
+          </span>
         </li>
       </ul>
   </div>
@@ -15,6 +18,11 @@ export default {
   data: function() {
     return {
       todoItems: []
+    }
+  },
+  methods: {
+    removeTodo: function(){
+
     }
   },
   // created 뷰 라이프 사이클 중 하나, 뷰 인스턴스가 생성되자마자 호출되는 훅, 생성되는 시점에 이안에 로직이 호출됨 
@@ -32,5 +40,36 @@ export default {
 </script>
 
 <style>
-
+ul {
+  list-style-type: none;
+  padding-left: 0px;
+  margin-top: 0;
+  text-align: left;
+}
+li{
+  display: flex;
+  min-height: 50px;
+  height: 50px;
+  line-height: 50px;
+  margin: 0.5rem 0;
+  padding: 0 0.9rem;
+  background: white;
+  border-radius: 5px;
+}
+.removeBtn{
+  margin-left: auto;
+  color: #b3adad;
+}
+.checkBtn{
+  line-height: 45px;
+  color: #62acde;
+  margin-right: 5px;
+}
+.checkBtnCompleted{
+  color: #b3adad;
+}
+.textCompleted {
+  text-decoration: line-through;
+  color: #b3adad;
+}
 </style>
